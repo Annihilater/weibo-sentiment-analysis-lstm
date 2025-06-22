72cfbc5447f78965dba75fde48fd499e9abc7d68
Annihilater <9960889@gmail.com>
2025-06-22 21:53:46 +0800

    Feature: 在start.sh中新增环境变量配置以屏蔽macOS输入法调试信息

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ echo "程序: 开始"
 # 创建输出目录
 mkdir -p data/output
 
+# 屏蔽 macOS 的输入法调试信息
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export PYTHON_IGNORE_DEPRECATION=1
+
 python src/main.py
 
 echo "程序: 完成"
